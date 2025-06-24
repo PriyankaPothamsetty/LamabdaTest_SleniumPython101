904b51e718dfbc595972a6f2e873263f11cc7d1c
PriyankaPothamsetty <131769413+PriyankaPothamsetty@users.noreply.github.com>
2025-06-24 08:59:32 +0530

    Update test_sampleformDemo.py

--- tests/test_sampleformDemo.py
+++ tests/test_sampleformDemo.py
@@ -2,6 +2,7 @@ from pages.Sample_Form_Demo_Page import SampleFormDemo
 from utilities.Test_Data import Test_Data
 from tests.base_test import BaseTest
 from utilities.locators import SampleFormDemolocators
+import conftest
 
 class TestSampleFormDemo(BaseTest):
 
@@ -13,6 +14,7 @@ class TestSampleFormDemo(BaseTest):
         Sampleform_page.enter_message(SampleFormDemolocators.Enter_Message_files, Test_Data.welcome_lambda)
         actual_text = Sampleform_page.output_text(SampleFormDemolocators.get_Message_filed)
         assert actual_text == Test_Data.welcome_lambda
+        conftest.change_test_status(self.driver , actual_text == Test_Data.welcome_lambda)
 
 
 
