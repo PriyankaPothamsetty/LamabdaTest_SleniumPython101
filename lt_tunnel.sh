313f73f771e36d3af4233ec12a7f22cfaec5e94e
PriyankaPothamsetty <131769413+PriyankaPothamsetty@users.noreply.github.com>
2025-06-24 08:53:17 +0530

    Add files via upload

---
+++ lt_tunnel.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Download LT Tunnel
+wget -q https://downloads.lambdatest.com/tunnel/v3/linux/64bit/LT -O LT
+chmod +x LT
+
+# Run the tunnel (replace with your LambdaTest credentials)
+./LT --user priyankapothamsetty@gmail.com --key LT_hoQsTu0nmHmQKmpqPyhtxa5ZrZCFFt8C0FYxwxLUwoyCjGJ
